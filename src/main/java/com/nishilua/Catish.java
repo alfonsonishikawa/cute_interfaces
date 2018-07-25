53870b8265485bbb1c15c569bfa987d8d30475db
Alfonso Nishikawa <alfonso.nishikawa@gmail.com>
2018-07-25 15:58:36 -0100

    Added Catish interface. Minor fixes.

---
+++ src/main/java/com/nishilua/Catish.java
@@ -0,0 +1,11 @@
+package com.nishilua;
+
+public interface Catish {
+
+	void meow() ;
+	
+	void purr() ;
+	
+	void purrpurr() ;
+	
+}
--- src/main/java/com/nishilua/Mushy.java
+++ src/main/java/com/nishilua/Mushy.java
@@ -1,4 +1,4 @@
-
+package com.nishilua;
 
 public interface Mushy {
 
